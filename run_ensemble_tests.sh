17d3a0d4ba5217d1e4be4f10607c44b2ff5ddb37
Yusoi <bernardosilva.business@gmail.com>
2022-02-01 19:07:32 +0000

    tests

--- run_ensemble_tests.sh
+++ run_ensemble_tests.sh
@@ -6,11 +6,11 @@ for DATASET in coco
 do
     for ENSEMBLE_METHOD in "network"
     do
-        for CHECKPOINT in "work_dirs/simplenet_1/sigmoid_BCELoss/epoch_25.pt"
+        for CHECKPOINT in "work_dirs/simplenet_1_1/sigmoid_BCELoss/epoch_25.pt"
         do
         
             python run_evaluation.py "$ORDERED" -data "$DATASET" -e "$ENSEMBLE_METHOD" -ck "$CHECKPOINT" \
-                                        > outputs/network/results/simplenet_1.txt
+                                        > outputs/network/results/simplenet_1_1.txt
         done
     done
 done
